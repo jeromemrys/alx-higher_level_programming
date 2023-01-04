e59dcfe734294cacd6cbdf8e5d58eb89b382c8e4
jeromemrys <jeromemrys@gmail.com>
2023-01-03 16:11:33 -0800

    Task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    remainder = -1 * (abs(number) % 10)
+else:
+    remainder = number % 10
+print(f"last digit of {number} is", end= ' ')
+if remainder > 5:
+    print(f"{remainder} and is greater than 5")
+elif remainder == 0:
+    print(f"{remainder} and is 0")
+else:
+    print(f"{remainder} and is less than 6 and not 0")
+
